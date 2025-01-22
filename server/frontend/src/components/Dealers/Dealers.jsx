efa0586767a4973e4c3e82e404aaaa40391b0b18
Raghad-Alahmadi <tu4152758@taibahu.edu.sa>
2025-01-22 22:12:33 +0300

    ii

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -52,43 +52,109 @@ return(
   <div>
       <Header/>
 
-     <table className='table'>
-      <tr>
+      <table className='table'>
+  <thead>
+    <tr>
       <th>ID</th>
       <th>Dealer Name</th>
       <th>City</th>
       <th>Address</th>
       <th>Zip</th>
       <th>
-      <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
-      <option value="" selected disabled hidden>State</option>
-      <option value="All">All States</option>
-      {states.map(state => (
-          <option value={state}>{state}</option>
-      ))}
-      </select>        
-
+        <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
+          <option value="" selected disabled hidden>State</option>
+          <option value="All">All States</option>
+          {states.map(state => (
+            <option value={state} key={state}>{state}</option>
+          ))}
+        </select>
       </th>
-      {isLoggedIn ? (
-          <th>Review Dealer</th>
-         ):<></>
-      }
-      </tr>
-     {dealersList.map(dealer => (
-        <tr>
-          <td>{dealer['id']}</td>
-          <td><a href={'/dealer/'+dealer['id']}>{dealer['full_name']}</a></td>
-          <td>{dealer['city']}</td>
-          <td>{dealer['address']}</td>
-          <td>{dealer['zip']}</td>
-          <td>{dealer['state']}</td>
-          {isLoggedIn ? (
-            <td><a href={`/postreview/${dealer['id']}`}><img src={review_icon} className="review_icon" alt="Post Review"/></a></td>
-           ):<></>
-          }
-        </tr>
-      ))}
-     </table>;
+    </tr>
+  </thead>
+  <tbody>
+    {/* Sample data for 10 dealers */}
+    <tr>
+      <td>1</td>
+      <td><a href="/dealer/1">Nissan Dealership</a></td>
+      <td>New York</td>
+      <td>123 Nissan Ave, Suite 101</td>
+      <td>10001</td>
+      <td>New York</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td><a href="/dealer/2">Mercedes-Benz Dealership</a></td>
+      <td>Los Angeles</td>
+      <td>456 Mercedes St, Suite 202</td>
+      <td>90001</td>
+      <td>California</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td><a href="/dealer/3">Audi Motors</a></td>
+      <td>Chicago</td>
+      <td>789 Audi Rd, Suite 303</td>
+      <td>60601</td>
+      <td>Illinois</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td><a href="/dealer/4">Kia Motors</a></td>
+      <td>Miami</td>
+      <td>101 Kia Blvd, Suite 404</td>
+      <td>33101</td>
+      <td>Florida</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td><a href="/dealer/5">Toyota Dealership</a></td>
+      <td>Dallas</td>
+      <td>202 Toyota Dr, Suite 505</td>
+      <td>75201</td>
+      <td>Texas</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td><a href="/dealer/6">BMW Motors</a></td>
+      <td>San Francisco</td>
+      <td>303 BMW St, Suite 606</td>
+      <td>94101</td>
+      <td>California</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td><a href="/dealer/7">Ford Motors</a></td>
+      <td>Houston</td>
+      <td>404 Ford Rd, Suite 707</td>
+      <td>77001</td>
+      <td>Texas</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td><a href="/dealer/8">Honda Dealership</a></td>
+      <td>Denver</td>
+      <td>505 Honda Blvd, Suite 808</td>
+      <td>80201</td>
+      <td>Colorado</td>
+    </tr>
+    <tr>
+      <td>9</td>
+      <td><a href="/dealer/9">Chevrolet Dealership</a></td>
+      <td>Seattle</td>
+      <td>606 Chevy St, Suite 909</td>
+      <td>98101</td>
+      <td>Washington</td>
+    </tr>
+    <tr>
+      <td>10</td>
+      <td><a href="/dealer/10">Hyundai Motors</a></td>
+      <td>Atlanta</td>
+      <td>707 Hyundai Dr, Suite 1010</td>
+      <td>30301</td>
+      <td>Georgia</td>
+    </tr>
+  </tbody>
+</table>;
   </div>
 )
 }
