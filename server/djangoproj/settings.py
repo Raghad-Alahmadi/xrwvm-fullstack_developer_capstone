608ddb35696906f6699814fb1834740c1c329c9c
Raghad-Alahmadi <tu4152758@taibahu.edu.sa>
2025-01-22 19:26:23 +0300

    11

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://raghaddadnan-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS=['<https://raghaddadnan-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai>']
 
 REST_FRAMEWORK = {
@@ -65,6 +65,7 @@ TEMPLATES = [
             os.path.join(BASE_DIR, 'frontend/static'),
             os.path.join(BASE_DIR, 'frontend/build'),
             os.path.join(BASE_DIR, 'frontend/build/static'),
+
         ],
         'APP_DIRS': True,
         'OPTIONS': {
